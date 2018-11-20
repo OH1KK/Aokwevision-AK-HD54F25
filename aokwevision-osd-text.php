221cef92d23bde3cb805e52ad92ad0596a9e5e05
Kari Karvonen <github@toimii.fi>
2018-11-20 21:43:30 +0200

    Create aokwevision-osd-text.php

---
+++ aokwevision-osd-text.php
@@ -0,0 +1,40 @@
+#!/usr/bin/php -q
+<?php
+
+$cam_user="admin";
+$cam_password="";
+$cam_ip="192.168.1.110";
+
+function WriteOverlay1($OSD1Str, $Text1OSDEnable=1, $Text1OSDX=2, $Text1OSDY=2, $OSD1Size=3) {
+    global $cam_ip, $cam_user, $cam_passwd;
+    $url="http://".$cam_ip."/form/OSDSet";
+    $headers=array(
+        'Authorization: Basic '.base64_encode($cam_user.":".$cam_passwd),
+        'Content-Type: application/x-www-form-urlencoded',
+        'X-Requested-With: XMLHttpRequest'
+    );
+    $data = ""
+    ."Text1OSDEnable=${Text1OSDEnable}&"
+    ."Text1OSDX=${Text1OSDX}&"
+    ."Text1OSDY=${Text1OSDY}&"
+    ."OSD1Size=${OSD1Size}&"
+    ."OSD1Str=${OSD1Str}";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $return = curl_exec($ch);
+    curl_close($ch);
+}
+
+if (isset($_SERVER["argv"]["1"])) {
+    $overlaytext=substr(filter_var($_SERVER["argv"]["1"],FILTER_SANITIZE_STRING),0,35);
+    WriteOverlay1($overlaytext);
+} else {
+    echo "Usage: ".$_SERVER["argv"]["0"]." \"blah blah blah\"\n";
+    echo "\n";
+    echo "maximum text length is 36 chars\n";
+}
